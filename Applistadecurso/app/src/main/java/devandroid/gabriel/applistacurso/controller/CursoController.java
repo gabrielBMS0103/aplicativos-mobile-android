7857e90523c8af3589a8afa73a88e8f622f70df5
gabrielBMS0103 <gabriiel45.barbosa@gmail.com>
2023-07-18 17:21:46 -0300

    Arquitetura MVC

---
+++ Applistadecurso/app/src/main/java/devandroid/gabriel/applistacurso/controller/CursoController.java
@@ -0,0 +1,4 @@
+package devandroid.gabriel.applistacurso.controller;
+
+public class CursoController {
+}
--- Applistadecurso/app/src/main/java/devandroid/gabriel/applistacurso/view/MainActivity.java
+++ Applistadecurso/app/src/main/java/devandroid/gabriel/applistacurso/view/MainActivity.java
@@ -1,8 +1,10 @@
-package devandroid.gabriel.applistacurso;
+package devandroid.gabriel.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import devandroid.gabriel.applistacurso.R;
 
 public class MainActivity extends AppCompatActivity {
 
